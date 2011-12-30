34e961186a25ea978e3e73923eb1c7c645c1c17e
Sam Cavenagh <sam@Punk.local>
2011-12-29 21:39:45 -0800

    load photo description on hover

--- beer.js
+++ beer.js
@@ -1,6 +1,50 @@
 $(function(){
 
+  var API_KEY = "ef9ad4ef689af505cde45ec1dc31120f";
+  var BASE_FLICKR_URL = "http://api.flickr.com/services/rest/?"
+
   window.Photo = Backbone.Model.extend({
+
+    photoPopoverTemplate: _.template($('#photo-popover-content').html()),
+    spinnerTemplate: _.template($('#spinner-template').html()),
+
+    url: function(){
+      var params = {
+        "method"         : 'flickr.photos.getInfo',
+        "api_key"        : API_KEY,
+        "photo_id"       : this.id,
+        "format"         : 'json',
+        "nojsoncallback" : 1
+      }
+      return _.reduce(
+        params,
+        function(url, value, key) { return url + key + '=' + value + '&' },
+        BASE_FLICKR_URL
+      );
+    },
+
+    parse: function(response) {
+      this.attributes.details_loaded = true;
+      this.attributes.description = response.photo.description._content;
+
+      return this.attributes
+    },
+
+    popoverContents: function(){
+      if(this.get('details_loaded')){
+        return this.photoPopoverTemplate({ 'photo':this });
+      }
+
+      console.log("Loading " + this.id + " details");
+      var photo = this;
+      this.fetch({
+        success: function(){
+          $('#spinner' + photo.id).replaceWith(photo.popoverContents());
+        }
+      });
+
+      return this.spinnerTemplate({ 'spinnerId': 'spinner' + this.id })
+    }
 
   });
 
@@ -9,23 +53,24 @@ $(function(){
 
     url: function(){
       var params = {
-        "method"         : "flickr.photosets.getPhotos",
-        "api_key"        : 'ef9ad4ef689af505cde45ec1dc31120f',
+        "method"         : 'flickr.photosets.getPhotos',
+        "api_key"        : API_KEY,
         "photoset_id"    : '72157625277593652',
-        "extras"         : 'date_taken%2Curl_t%2Curl_m%2Curl_s',
+        "extras"         : 'date_taken%2Curl_t%2Curl_m%2Curl_s%2Ctags',
         "format"         : 'json',
         "nojsoncallback" : 1
       }
       return _.reduce(
         params,
         function(url, value, key) { return url + key + '=' + value + '&' },
-        "http://api.flickr.com/services/rest/?"
+        BASE_FLICKR_URL
       );
     },
 
     parse: function(response){
       return response.photoset.photo;
-    }
+    },
+
   });
 
   window.Photos = new PhotoSet();
@@ -33,8 +78,6 @@ $(function(){
   window.GatewayView = Backbone.View.extend({
 
     el: $("#backbone-beer-app"),
-
-    photoGatewayTemplate: _.template($('#photo-gateway-template').html()),
 
     initialize: function() {
       console.log("Init Gateway View");
@@ -53,12 +96,14 @@ $(function(){
         }));
       });
 
+      // Add popover to photos
       this.$("a[rel=photoPopover]")
         .popover({
           offset: 10,
           html: true,
           content: function() {
-            return $('#spinner-template').html();
+            var photo = Photos.get($(this).data('photo-id'));
+            return photo.popoverContents();
           }
       });
 
